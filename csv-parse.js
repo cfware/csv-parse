cf15cf920b26407b8be2324cd9a41856360eb051
Corey Farrell <git@cfware.com>
2023-10-15 12:11:08 -0400

    fix!: Update development dependencies

--- csv-parse.js
+++ csv-parse.js
@@ -1,47 +1,47 @@
 export default (string, maxRows = Infinity) => {
-	let inQuote = false;
-	let field = '';
-	let row = [];
-	const result = [];
-	const charArray = [
-		// Chomp final line terminator
-		...string.replace(/(?:\r\n|\n|\r)$/u, '')
-	];
+    let inQuote = false;
+    let field = '';
+    let row = [];
+    const result = [];
+    const charArray = [
+        // Chomp final line terminator
+        ...string.replace(/(?:\r\n|\n|\r)$/u, '')
+    ];
 
-	for (let index = 0; index < charArray.length; index++) {
-		const current = charArray[index];
-		const next = charArray[index + 1];
-		if (!inQuote && ',\r\n'.includes(current)) {
-			row.push(field);
-			field = '';
+    for (let index = 0; index < charArray.length; index++) {
+        const current = charArray[index];
+        const next = charArray[index + 1];
+        if (!inQuote && ',\r\n'.includes(current)) {
+            row.push(field);
+            field = '';
 
-			if (current !== ',') {
-				if (current + next === '\r\n') {
-					index++;
-				}
+            if (current !== ',') {
+                if (current + next === '\r\n') {
+                    index++;
+                }
 
-				result.push(row);
-				if (result.length >= maxRows) {
-					return result;
-				}
+                result.push(row);
+                if (result.length >= maxRows) {
+                    return result;
+                }
 
-				row = [];
-			}
-		} else if (current !== '"') {
-			field += current;
-		} else if (!inQuote) {
-			inQuote = true;
-		} else if (next === '"') {
-			field += '"';
-			index++;
-		} else {
-			inQuote = false;
-		}
-	}
+                row = [];
+            }
+        } else if (current !== '"') {
+            field += current;
+        } else if (!inQuote) {
+            inQuote = true;
+        } else if (next === '"') {
+            field += '"';
+            index++;
+        } else {
+            inQuote = false;
+        }
+    }
 
-	// Add the last field
-	row.push(field);
-	result.push(row);
+    // Add the last field
+    row.push(field);
+    result.push(row);
 
-	return result;
+    return result;
 };
--- test.js
+++ test.js
@@ -2,35 +2,35 @@ import t from 'libtap';
 import parse from './csv-parse.js';
 
 const expected = [
-	['Jones,\r\n \n \rJay', '10'],
-	['Xyz "ABC" O\'Brien', '11:35'],
-	['Other, AN', '12:35']
+    ['Jones,\r\n \n \rJay', '10'],
+    ['Xyz "ABC" O\'Brien', '11:35'],
+    ['Other, AN', '12:35']
 ];
 
 t.same(
-	parse(
-		'"Jones,\r\n \n \rJay",10\r\n' +
-		'"Xyz ""ABC"" O\'Brien",11:35\n' +
-		'"Other, AN",12:35\r'
-	),
-	expected
+    parse(
+        '"Jones,\r\n \n \rJay",10\r\n' +
+        '"Xyz ""ABC"" O\'Brien",11:35\n' +
+        '"Other, AN",12:35\r'
+    ),
+    expected
 );
 
 t.same(
-	parse(
-		'"Jones,\r\n \n \rJay",10\n' +
-		'"Xyz ""ABC"" O\'Brien",11:35\n' +
-		'"Other, AN",12:35'
-	),
-	expected
+    parse(
+        '"Jones,\r\n \n \rJay",10\n' +
+        '"Xyz ""ABC"" O\'Brien",11:35\n' +
+        '"Other, AN",12:35'
+    ),
+    expected
 );
 
 t.same(
-	parse(
-		'"Jones,\r\n \n \rJay",10\n' +
-		'"Xyz ""ABC"" O\'Brien",11:35\n' +
-		'"Other, AN",12:35',
-		2
-	),
-	expected.slice(0, 2)
+    parse(
+        '"Jones,\r\n \n \rJay",10\n' +
+        '"Xyz ""ABC"" O\'Brien",11:35\n' +
+        '"Other, AN",12:35',
+        2
+    ),
+    expected.slice(0, 2)
 );
